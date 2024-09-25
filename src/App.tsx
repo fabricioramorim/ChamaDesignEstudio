cc0027129d42951dc2e454d1b0a44112b44604ad
fabrioceras <fabrioceras@gmail.com>
2024-09-25 20:42:08 +0000

    First-commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import LogoChama from '/Logo_chama.png'
+import SeloChama from '/Selo_chama_pro_job.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <img src={LogoChama} className="logo" alt="Vite logo" />
+      </div>
+      <h1>Site em construção...</h1>
+      <div>
+      <img src={SeloChama} className="logo" alt="Vite logo" />
+      </div>
+      <div>
+      <h2>Nos acompanhe</h2>
+      <h2>Chama Design Estúdio</h2>
+      </div>      
+    </>
+  )
+}
+
+export default App
